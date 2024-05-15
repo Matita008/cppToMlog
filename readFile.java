8d23a990b762cebdf2157d166647f2112b93a934
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-05-15 22:05:19 +0200

    Update readFile.java

--- readFile.java
+++ readFile.java
@@ -10,11 +10,11 @@ public class ReadFile {
       outFile.write("File updated!");
       try {
         File inFile = new File("inFile.txt");
-				Scanner read = new Scanner(inFile);
-				while (read.hasNextLine()) {
-					String line = read.nextLine;
-					outFile.write(line + "\n");
-					System.out.println(line);
+        Scanner read = new Scanner(inFile);
+        while (read.hasNextLine()) {
+          String line = read.nextLine;
+          outFile.write(line + "\n");
+          System.out.println(line);
 				}
 			} catch (IOExeption e) {
         e.printStackTrace();
