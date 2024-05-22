d5c51f29f024361524e6e9db1cf9056fc1373718
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-05-22 18:23:40 +0200

    Update stringManager.java

--- stringManager.java
+++ stringManager.java
@@ -1,17 +1,19 @@
 public class stringManager {
-    string data;
+    String dataSt;
+    char[] data;
     int length;
-    stringManager(string data)  {
-        this.data = data;
-        this.lenght = data.length();
+    stringManager(String data)  {
+        this.dataSt = data;
+        this.length = data.length();
+        this.data = this.dataSt.toChar();
     }
-    int public final countOcc(char[][] value,int nChar,char any = "")  {
+    public final int countOcc(char[][] value,int nChar,char any)  {
         int count = 0;
         for(int i = 0; i < length - nChar; i++)  {
-            bool done = false;
-            for each(char val[] : value)  {
+            boolean done = false;
+            for (char val[] : value)  {
             for(int n = 0; n < nChar; n++)  {
-                if(done || )   n = nChar + 1;
+                if(done || val[n] != this.data[i+n])   n = nChar + 1;
                 if(n == nChar) done = true;
               }
             }
