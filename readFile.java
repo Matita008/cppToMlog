cadee0a4bf0312ec83a679df825a41cd0a6423a6
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-06-01 14:23:43 +0200

    added launch args

--- readFile.java
+++ readFile.java
@@ -1,42 +1,70 @@
 import java.io.*;
-import utilities.*;
 import java.util.*;
-/*import java.io.FileNotFoundException;
-import java.io.File;
-import java.util.Scanner;*/
+import utilities.*;
 
 public class readFile {
-  public static void main(String[] args) {
-    try {
-      File outFileCk = new File("outFile.mlog");
-      if (!outFileCk.createNewFile())  System.out.println("file exisit");
-      else System.out.println("File created automatically");
-      try {
-          FileWriter outFile = new FileWriter("outFile.mlog");
-          FileWriter workFile = new FileWriter("workFile.txt");
-          workFile.write("New file recived!");
-        try {
-          File inFile = new File("inFile.txt");
-          	Scanner read = new Scanner(inFile);
-	          while (read.hasNextLine()) {
-	            String line = read.nextLine();
-	            stringManager str = new stringManager(line);
-	            outFile.write(str.countOp() + " operation\t");
-	            outFile.write(line + "\n");
-	            System.out.print(str.countOp() + " operation\t");
-	            System.out.println(line);
-	          }
-	          read.close();
-        } catch (FileNotFoundException  e) {
-          e.printStackTrace();
-        }
-        workFile.close();
-        outFile.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	public static void main(String[] args) {
+		String inFileName = "inFile.cpp";
+		String workFileName = "workFile.txt";
+		String outFileName = "outFile.mlog";
+		if (args.length == 0) {
+		} else if (args[0].charAt(0) == '-') {
+			for (int i = 0; i < args.length; i++) {
+				if (!(args[i].charAt(0) == '-'))
+					throw new RuntimeException("need '-' before a parameter letter");
+				switch (args[i].charAt(1)) {
+				case 'i':
+					inFileName = args[i++];
+					break;
+				case 'o':
+					outFileName = args[i++];
+					break;
+				case 'w':
+					workFileName = args[i++];
+					break;
+				}
+			}
+		} else if (args.length == 1) {
+			inFileName = args[0];
+		} else if (args.length == 2) {
+			inFileName = args[0];
+			outFileName = args[1];
+		} else if (args.length == 3) {
+			inFileName = args[0];
+			workFileName = args[1];
+			outFileName = args[2];
+		}
+		try {
+			File outFileCk = new File(outFileName);
+			File workFileCk = new File(workFileName);
+			if (outFileCk.createNewFile())
+				System.out.println("Output file created automatically");
+			else
+				System.out.println("Output file arleady exisit");
+			if (workFileCk.createNewFile())
+				System.out.println("Work file created automatically");
+			FileWriter outFile = new FileWriter(outFileName);
+			FileWriter workFile = new FileWriter(workFileName);
+			workFile.write("New file recived!");
+			try {
+				File inFile = new File(inFileName);
+				Scanner read = new Scanner(inFile);
+				while (read.hasNextLine()) {
+					String line = read.nextLine();
+					stringManager str = new stringManager(line);
+					outFile.write(str.countOp() + " operation\t");
+					outFile.write(line + "\n");
+					System.out.print(str.countOp() + " operation\t");
+					System.out.println(line);
+				}
+				read.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			workFile.close();
+			outFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
