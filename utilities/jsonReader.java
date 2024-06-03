80ce6f2886c02f54fc61e566b1c93ee2bfc005ec
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-06-03 23:09:49 +0200

    Create jsonReader.java

---
+++ utilities/jsonReader.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 
+ */
+public final class jsonReader {
+	private File file;
+	@SuppressWarnings("unused")
+	private HashMap<String, Object> content = new HashMap<String, Object>();
+
+	/*
+	 * public jsonReader(String name) { //File file = new File(name); this(name ->
+	 * File::File(name)); }
+	 */
+
+	public jsonReader(File file) {
+		this.file = file;
+		scanFile();
+	}
+
+	private void scanFile() throws FileNotFoundException {
+		Scanner scanner = new Scanner(file);
+		scanFile(scanner);
+	}
+
+	private void scanFile(Scanner scanner) {
+
+	}
+}
