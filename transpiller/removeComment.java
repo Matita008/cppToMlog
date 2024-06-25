bf13b6e66eff1267183ffcac78f2443aed798479
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-06-25 22:44:42 +0200

    added void removeSingleLane(FileWriter) & contructor removeComment(String,String) to transpiller\removeComment.java

--- transpiller/removeComment.java
+++ transpiller/removeComment.java
@@ -23,18 +23,31 @@ public class removeComment {
 		this.out = out;
 	}
 
+	removeComment(String in, String out) throws IOException {
+		this.in = new File(in);
+		this.out = new FileWriter(out);
+	}
+
 	public void removeMultiLane() throws IOException {
 		Scanner read = new Scanner(in);
 		while (read.hasNextLine()) {
 			String line = read.nextLine();
-			if(line.contains("\\*")) {
-				
+			if (line.contains("\\*")) {
+
 			}
 		}
 		read.close();
 	}
 
-	public void removeSingleLane() {
-
+	public void removeSingleLane(FileWriter output) throws IOException {
+		Scanner read = new Scanner(in);
+		while (read.hasNextLine()) {
+			String line = read.nextLine();
+			if (!line.startsWith("\\")) {
+				output.write(line);
+			}
+		}
+		read.close();
+		output.close();
 	}
 }
