530254762f9d331b8f99cbfa1762768a83d9c2c3
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-08-09 19:17:41 +0200

    update varHandler : better getName() + code cleanup

--- transpiller/varHandler.java
+++ transpiller/varHandler.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class varHandler {
 	private static ArrayList<String> name = new ArrayList<String>();
-	private static HashMap<Integer, ArrayList<Integer>> child = new HashMap<Integer, ArrayList<Integer>>();
-	private static HashMap<Integer, ArrayList<Integer>> parent1 = new HashMap<Integer, ArrayList<Integer>>();
+	public static HashMap<Integer, ArrayList<Integer>> child = new HashMap<Integer, ArrayList<Integer>>();
+	public static HashMap<Integer, ArrayList<Integer>> parent = new HashMap<Integer, ArrayList<Integer>>();
 	private static int curScope = 0;
 	private static int maxScope = 0;
 
 	private static Boolean isValideType(String type) {
-		if (type == "Array")
+		if (type == "Array") 
 			throw new RuntimeException("Arrays aren't supported rn bc i want to focus onto the rest");
 		// TODO Plz implement me ;)
 		if (type == "Var" || type == "String" || type == "Bool" || type == "Object" || type == "Type" || type == "Int"
@@ -28,21 +28,21 @@ public class varHandler {
 		ArrayList<Integer> c = new ArrayList<Integer>(child.get(curScope));
 		c.add(curScope);
 		child.replace(curScope, child.get(curScope), c);
-		ArrayList<Integer> p = new ArrayList<Integer>(parent1.get(curScope));
+		ArrayList<Integer> p = new ArrayList<Integer>(parent.get(curScope));
 		p.add(curScope);
-		parent1.put(curScope, p);
+		parent.put(curScope, p);
 		maxScope++;
 		curScope = maxScope;
 	}
 
 	public static void closeScope() {
-		curScope = parent1.get(curScope).get(parent1.get(curScope).size() - 1);
+		curScope = parent.get(curScope).get(parent.get(curScope).size() - 1);
 	}
 
 	public static String getName(String name, int Scope, String Type) {
-		if (!isValideType(Type) || Scope > maxScope)
+		if (!isValideType(Type) || Scope > maxScope || name.replace(" ", "") == "" || name.contains("_"))
 			throw new RuntimeException("an error occurred");// TODO
-		return name + "_" + Scope + "_" + Type;
+		return name.replace(" ", "") + (curScope == 0 ? "" : "_" + Scope) + ( Type == "" || Type == null ? "" : "_" + Type);
 	}
 
 	public static boolean addVar(String name) {
@@ -69,6 +69,6 @@ public class varHandler {
 	}
 
 	public static boolean exist(String name, int Scope, String Type) {
-		return !isValideType(Type) || Scope > maxScope || ;
+		return !isValideType(Type) || Scope > maxScope || false;
 	}
 }
