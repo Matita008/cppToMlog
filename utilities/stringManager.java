196cdc9f5b2e3af6acc559149be31a18e2e394d3
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-08-09 22:27:17 +0200

    Update stringManager.java

--- utilities/stringManager.java
+++ utilities/stringManager.java
@@ -2,85 +2,64 @@ package utilities;
 
 import java.util.*;
 import java.io.*;
-import utilities.converter;
+//import utilities.converter;
 
 public class stringManager {
 	String dataSt;
 	char[] data;
 	int length;
 	int opLength = 3;
-	ArrayList<String> op = new ArrayList<String>();
+	ArrayList <String> op = new ArrayList <String>();
 
-	public stringManager(String data) {
+	public stringManager (String data) {
 		this.dataSt = data;
 		this.length = data.length();
 		this.data = this.dataSt.toCharArray();
 		loadBaseOp();
 	}
 
-	public final int countOp() {
+	public final int countOp () {
 		char empty = ' ';
 		int n = opLength;
-		@SuppressWarnings("unchecked")
-		char[][] ops = converter.toChar((ArrayList<String>) this.op.clone());
+		@SuppressWarnings ("unchecked")
+		char[][] ops = converter.toChar((ArrayList <String>) this.op.clone());
 		return countOcc(ops, n, empty);
 	}
 
-	public final int toInt() {
+	public final int toInt () {
 		int n = 0;
 		for (char c : data) {
-			int a;
-			switch (c) {
-			default:
-				a = 0;
-				break;
-			case '1':
-				a = 1;
-				break;
-			case '2':
-				a = 2;
-				break;
-			case '3':
-				a = 3;
-				break;
-			case '4':
-				a = 4;
-				break;
-			case '5':
-				a = 5;
-				break;
-			case '6':
-				a = 6;
-				break;
-			case '7':
-				a = 7;
-				break;
-			case '8':
-				a = 8;
-				break;
-			case '9':
-				a = 9;
-			}
-			if (a != 0)
-				n *= 10;
+			int a = switch (c) {
+				default -> 0;
+				case '1' -> 1;
+				case '2' -> 2;
+				case '3' -> 3;
+				case '4' -> 4;
+				case '5' -> 5;
+				case '6' -> 6;
+				case '7' -> 7;
+				case '8' -> 8;
+				case '9' -> 9;
+			};
+			if (a != 0) n *= 10;
 			n += a;
 		}
 		return n;
 	}
 
-	public final int countOcc(char[][] value) {
+	public final int countOcc (char[][] value) {
 		return countOcc(value, value[0].length, ' ');
 	}
 
-	public final int countOcc(char[][] value, char any) {
+	public final int countOcc (char[][] value, char any) {
 		return countOcc(value, value[0].length, any);
 	}
 
-	public final int countOcc(char[][] value, int nChar) {
+	public final int countOcc (char[][] value, int nChar) {
 		return countOcc(value, nChar, ' ');
 	}
 
-	public final int[] countSimb(char[][] value, char any) {
+	public final int[] countSimb (char[][] value, char any) {
 		int[] array = new int[value.length];
 		for (int i = 0; i < value.length; i++) {
 			char[][] tmp = { value[i] };
@@ -89,15 +68,14 @@ public class stringManager {
 		return array;
 	}
 
-	public final int countOcc(char[][] value, int nChar, char any) {
+	public final int countOcc (char[][] value, int nChar, char any) {
 		int count = 0;
 		for (int i = 0; i < length - nChar + 1; i++) {
 			for (int k = 0; k < value.length; k++) {
 				boolean found = true;
 				for (int n = 0; n < nChar; n++) {
-					if (value[k][n] == any)
-						continue;
-					if (!(value[k][n] == this.data[i + n])) {
+					if (value[k][n] == any) continue;
+					if (! (value[k][n] == this.data[i + n])) {
 						found = false;
 						break;
 					}
@@ -112,45 +90,39 @@ public class stringManager {
 		return count;
 	}
 
-	public final int getPos(char[][] val) {
+	public final int getPos (char[][] val) {
 		for (int i = 0; i < length - val[0].length; i++) {
 			for (int n = 0; n < val.length; n++) {
-				if (val[i][n] != this.data[i + n])
-					n = val.length + 1;
-				else if (n == val.length)
-					return i;
+				if (val[i][n] != this.data[i + n]) n = val.length + 1;
+				else if (n == val.length) return i;
 			}
 		}
 		String error = "in " + dataSt + " weren't found any of the following: ";
 		for (char[] v : val) {
-			for (char a : v)
-				error = error + a;
+			for (char a : v) error = error + a;
 			error = error + ",\n";
 		}
 		Throwable err = new Throwable(error);
 		throw new IOError(err);
 	}
 
-	public final int getOpPos(int index) {
+	public final int getOpPos (int index) {
 		var count = 0/* ,rtn = 0 */;
 		for (int i = 0; i < length - 3; i++) {
 			boolean done = false;
 			for (String opSt : op) {
 				char[] val = opSt.toCharArray();
 				for (int n = 0; n < 3; n++) {
-					if (done || val[n] != this.data[i + n])
-						n = 3 + 1;
-					if (n == 3)
-						done = true;
+					if (done || val[n] != this.data[i + n]) n = 3 + 1;
+					if (n == 3) done = true;
 				}
 			}
-			if (done)
-				count++;
+			if (done) count++;
 		}
 		return count;// TODO ???????
 	}
 
-	public final void loadBaseOp() {
+	public final void loadBaseOp () {
 		op.add(" + ");
 		op.add(" - ");
 		op.add(" * ");
