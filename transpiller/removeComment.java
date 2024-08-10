cfc3985884cd6a399e37bc04a01ee373f83f687f
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-08-10 20:13:04 +0200

    Update removeComment.java
    addad possibility of using the code + a bugfix?

--- transpiller/removeComment.java
+++ transpiller/removeComment.java
@@ -4,41 +4,47 @@ import java.io.*;
 import java.util.*;
 
 public class removeComment {
-	public static void MultiLane(File in, FileWriter out, String start, String end) throws IOException {
+	/*public static void main (String[] args) throws Exception {
+		@SuppressWarnings ("unused")
+		File a = new File("String.java");
+		@SuppressWarnings ("unused")
+		File b = new File("C:\\Users\\matti\\Documents\\GitHub\\cppToMlog\\transpiller\\f.n");
+		MultiLane(b, new FileWriter("String.java"));
+	}*/
+
+	public static void MultiLane (File in, FileWriter out, String start, String end) throws IOException {
 		Scanner read = new Scanner(in);
 		while (read.hasNextLine()) {
 			String line = read.nextLine();
 			if (line.contains(start)) {
-				out.write(line.codePointBefore(line.indexOf(start)) + "\n");
+				line = line + 1;
+				out.write(line.codePointBefore(line.indexOf(start) <= 1 || line.indexOf(start) + 1 >= line.length() ? 2 : line.indexOf(start) + 1) + "\n");
 				while (!line.contains(end)) {
 					line = read.nextLine();
 				}
 				out.write((String) line.subSequence(line.indexOf(end) + end.length(), line.length()) + "\n");
-			} else
-				out.write(line + "\n");
+			} else out.write(line + "\n");
 		}
 		read.close();
 		out.close();
 	}
 
-	public static void SingleLane(File in, FileWriter out, String prefix) throws IOException {
+	public static void SingleLane (File in, FileWriter out, String prefix) throws IOException {
 		Scanner read = new Scanner(in);
 		while (read.hasNextLine()) {
 			String line = read.nextLine();
-			if (line.contains(prefix))
-				out.write(line.codePointBefore(line.indexOf(prefix)) + "\n");
-			else
-				out.write(line + "\n");
+			if (line.contains(prefix)) out.write(line.codePointBefore(line.indexOf(prefix)) + "\n");
+			else out.write(line + "\n");
 		}
 		read.close();
 		out.close();
 	}
 
-	public static void MultiLane(File in, FileWriter out) throws Exception {
+	public static void MultiLane (File in, FileWriter out) throws Exception {
 		MultiLane(in, out, "/*", "*/");
 	}
 
-	public static void SingleLane(File in, FileWriter out) throws Exception {
+	public static void SingleLane (File in, FileWriter out) throws Exception {
 		SingleLane(in, out, "//");
 	}
 }
