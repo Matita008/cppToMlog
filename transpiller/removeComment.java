420968edd847ed58ba155876ef9eeca6d9f7bc59
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-08-18 13:56:57 +0200

    Update removeComment.java
    i need to learn list

--- transpiller/removeComment.java
+++ transpiller/removeComment.java
@@ -4,13 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class removeComment {
-	/*public static void main (String[] args) throws Exception {
-		@SuppressWarnings ("unused")
-		File a = new File("String.java");
-		@SuppressWarnings ("unused")
-		File b = new File("C:\\Users\\matti\\Documents\\GitHub\\cppToMlog\\transpiller\\f.n");
-		MultiLane(b, new FileWriter("String.java"));
-	}*/
+	/*
+	 * public static void main (String[] args) throws Exception {
+	 * @SuppressWarnings ("unused") File a = new File("String.java");
+	 * @SuppressWarnings ("unused") File b = new
+	 * File("C:\\Users\\matti\\Documents\\GitHub\\cppToMlog\\transpiller\\f.n");
+	 * MultiLane(b, new FileWriter("String.java")); }
+	 */
 
 	public static void MultiLane (File in, FileWriter out, String start, String end) throws IOException {
 		Scanner read = new Scanner(in);
@@ -39,6 +39,14 @@ public class removeComment {
 		read.close();
 		out.close();
 	}
+
+	/*
+	 * public static String[] SingleLane (String[] st, String prefix, boolean keep)
+	 * { List <String> out; for (String line : st) { if (line.contains(prefix))
+	 * out.add(line.substring(0, line.indexOf(prefix)) + (keep ?
+	 * line.substring(line.indexOf(prefix)) : "")); else out.add(line + ""); }
+	 * return (String[]) out.toArray(); }
+	 */// TODO
 
 	public static void MultiLane (File in, FileWriter out) throws Exception {
 		MultiLane(in, out, "/*", "*/");
